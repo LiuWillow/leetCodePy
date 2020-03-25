f55d56b09ee4ce8315e9069948becf12fb1963e7
liuwillow <liuwillow9625@163.com>
2020-03-25 10:47:12 +0800

    回文数

---
+++ Q9_isPalindrome.py
@@ -0,0 +1,38 @@
+# 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+# 示例 1:
+
+# 输入: 121
+# 输出: true
+# 示例 2:
+
+# 输入: -121
+# 输出: false
+# 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+# 示例 3:
+
+# 输入: 10
+# 输出: false
+# 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False 
+        if x > 0 and x < 10:
+            return True
+        if x % 10 == 0:
+            return False
+        right = 0
+
+        while x > right:
+            n = x % 10
+            right = right * 10 + n
+            x = int(x / 10)
+        return x == right or int(right / 10) == x
+
+print(Solution().isPalindrome(2332))
